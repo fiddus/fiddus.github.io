c884afd9af9a4e345a7e5c355d61c9634c44be33
Nathan Reuter <nathan0reuter@gmail.com>
2015-02-27 18:42:21 -0300

    test folder with website.

---
+++ Teste/scripts/pi.init.iCheck.js
@@ -0,0 +1,16 @@
+/* Aura version: 1.8.6 */
+
+jQuery(function($){
+
+	//region iCheck
+	if($.fn.iCheck){
+		$('input.pi-stylish-checkbox, input.pi-stylish-radio').iCheck({
+			checkboxClass: 'icheckbox_square',
+			radioClass: 'iradio_square',
+			focusClass: 'focus',
+			increaseArea: '20%' // optional
+		});
+	}
+	//endregion
+
+});
---
+++ Teste/scripts/pi.init.placeholder.js
@@ -0,0 +1,11 @@
+/* Aura version: 1.8.6 */
+
+jQuery(function($){
+
+	//region form placeholder
+	if($.fn.placeholder){
+		$('input, textarea').placeholder();
+	}
+	//endregion
+
+});
